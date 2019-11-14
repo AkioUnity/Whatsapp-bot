79e71ce58c58e8bd02d7c0fa29031b155a892dc4
an <mac@amgMac.local>
2019-11-14 12:34:32 +0700

    add multi language

--- src/global/i18n.js
+++ src/global/i18n.js
@@ -1,12 +1,21 @@
-import i18n from 'react-native-i18n';
-import pt from './locales/pt';
+import I18n from 'i18n-js';
+import * as RNLocalize from 'react-native-localize';
+
 import en from './locales/en';
+import pt from './locales/pt';
+import fr from './locales/fr';
 
-i18n.fallbacks = true;
+const locales = RNLocalize.getLocales();
 
-i18n.translations = {
+if (Array.isArray(locales)) {
+  I18n.locale = locales[0].languageTag;
+}
+
+I18n.fallbacks = true;
+I18n.translations = {
+  en,
+  fr,
   pt,
-  en
 };
 
-export default i18n;
+export default I18n;
